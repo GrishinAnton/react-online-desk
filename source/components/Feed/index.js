16f72cd459636f6c9cae8fada00ed4dc5785fed4
Grishin Anton <grishin_rostov@mail.ru>
2019-04-09 09:57:10 +0300

    Старт проекта

---
+++ source/components/Feed/index.js
@@ -0,0 +1,23 @@
+//Core
+import React, { Component } from 'react';
+
+// Components
+import StatusBar from '../StatusBar';
+import Composer from '../Composer';
+import Post from '../Post';
+
+// Instruments
+import Styles from './styles.m.css';
+
+
+export default class Feed extends Component {
+    render () {
+        return (
+            <section className = { Styles.feed }>
+                <StatusBar />
+                <Composer />
+                <Post />
+            </section>
+        );
+    }
+}
